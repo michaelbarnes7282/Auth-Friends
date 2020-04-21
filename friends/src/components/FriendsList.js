89d0930eadf9bd40370f3484c5cfeee940e35c5a
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-04-21 15:41:06 -0700

    finished MVP

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -5,7 +5,13 @@ import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 class FriendsList extends React.Component {
     state = {
-        friends: []
+        friends: [],
+        newFriend: {
+            id: '',
+            name: '',
+            age: '',
+            email: '',
+        }
     };
 
     componentDidMount() {
@@ -24,12 +30,67 @@ class FriendsList extends React.Component {
             .catch(err => console.log({ err }));
     }
 
+    handleChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name
+        this.setState({
+            ...this.state,
+            newFriend: {
+                ...this.state.newFriend,
+                id: Date.now(),
+                [name]: value
+            }
+        })
+
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state)
+        axiosWithAuth()
+            .post('/api/friends', this.state.newFriend)
+            .then(res => {
+                this.setState({
+                    ...this.state,
+                    friends: [
+                        ...this.state.friends,
+                        this.state.newFriend
+                    ]
+                })
+            })
+            .catch(err => console.log({ err }))
+    }
+
     render() {
         console.log('this state', this.state)
         return (
             <div>
                 <h1>Friends List</h1>
-                
+                <form onSubmit={this.handleSubmit}>
+                    <label>Name:
+                    <input
+                            type="text"
+                            name='name'
+                            value={this.state.newFriend.value}
+                            onChange={this.handleChange} />
+                    </label>
+                    <label>Age:
+                    <input
+                            type="text"
+                            name='age'
+                            value={this.state.newFriend.value}
+                            onChange={this.handleChange} />
+                    </label>
+                    <label>Email:
+                    <input
+                            type="text"
+                            name='email'
+                            value={this.state.newFriend.value}
+                            onChange={this.handleChange} />
+                    </label>
+                    <input type='submit' value='Submit' />
+                </form>
                 <div className='cards'>
                     {this.state.friends.map(friend => (
                         <div className='friend-card' key={friend.id}>
